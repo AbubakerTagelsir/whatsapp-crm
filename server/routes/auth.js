c9438ae9c8a3853342d5bbea6883b71d0e71202c
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2021-01-28 16:34:39 +0400

    restructuring routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,23 +1,21 @@
 const { verifySignUp } = require("../middlewares");
 const authControllers = require("../controllers/auth");
+const router = require("express").Router();
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
-    res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-  });
-
-  app.post(
-    "/api/auth/signup",
-    [
-      verifySignUp.checkDuplicateUsernameOrEmail,
-      verifySignUp.checkRolesExisted
-    ],
-    authControllers.signup
+router.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
   );
+  next();
+});
 
-  app.post("/api/auth/signin", authControllers.signin);
-};
+router.post(
+  "/signup",
+  [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted],
+  authControllers.signup
+);
+
+router.post("/signin", authControllers.signin);
+
+module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 
+const authRoutes = require('./auth');
+const testRoutes = require('./test');
+
+router.use('/auth', authRoutes);
+
+router.use('/test', testRoutes);
+
 module.exports = router;
