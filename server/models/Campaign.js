652ee880360d9facce208184e6368d7690f8694a
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2021-01-31 00:13:21 +0400

    campaign model

---
+++ server/models/Campaign.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const CampaignsSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    recipients: [
+        {
+            customerId:{
+                type: mongoose.Types.ObjectId,
+                ref: 'customers'
+            }
+        }
+    ],
+    body: {
+        type: String,
+        required: true,
+    },
+    sendingTime: {
+        type: Date,
+        default: new Date,
+    },
+    notes: {
+        type: String,
+    },
+  });
+
+module.exports = mongoose.model('customers', CampaignsSchema);
