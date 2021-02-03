6178b75ad6c48481db87f7b1bc1a3bf6d354bba8
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2021-02-03 13:33:59 +0400

    heroku deployment scripts

--- server/server.js
+++ server/server.js
@@ -13,7 +13,10 @@ const corsOptions = {
     origin: "http://localhost:3000"
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+const buildPath = path.join(__dirname, '..', 'client/build');
+app.use(express.static(buildPath));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
