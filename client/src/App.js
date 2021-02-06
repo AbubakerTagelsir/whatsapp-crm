ccf3854c4a0880d8cfe9ffb0e0b9fd39c078c16a
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2021-02-06 18:30:20 +0400

    theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,34 @@
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Fragment, Suspense, lazy } from "react";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import theme from "./theme";
+import GlobalStyles from "./GlobalStyles";
+import Pace from "./shared/components/Pace";
 
-import CreateCustomer from './components/create-customer';
-import EditCustomer from './components/edit-customer';
-import CustomersList from './components/list-customers';
-import logo from './logo.svg';
+const LoggedInComponent = lazy(() => import("./logged_in/components/Main"));
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
-            </a>
-            <Link to="/" className="navbar-brand">WhatsApp CRM</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                <Link to="/" className="nav-link">Campaigns</Link>
-                </li>
-                <li className="navbar-item">
-                <Link to="/" className="nav-link">Customers</Link>
-                </li>
-                <li className="navbar-item">
-                <Link to="/customer/create" className="nav-link">New Customer</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <Route path="/" exact component={CustomersList} />
-          <Route path="/customer/edit/:id" component={EditCustomer} />
-          <Route path="/customer/create" component={CreateCustomer} />
-        </div>
-      </Router>    );
-  }
+const LoggedOutComponent = lazy(() => import("./logged_out/components/Main"));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles />
+        <Pace color={theme.palette.primary.light} />
+        <Suspense fallback={<Fragment />}>
+          <Switch>
+            <Route path="/c">
+              <LoggedInComponent />
+            </Route>
+            <Route>
+              <LoggedOutComponent />
+            </Route>
+          </Switch>
+        </Suspense>
+      </MuiThemeProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
